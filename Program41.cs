0a2c16be4001bb8fb133ffbba307a57e65075fe9
Кузилин Юра <yurakuzilin@gmaio.com>
2023-07-04 19:04:46 +0300

    1

---
+++ Program41.cs
@@ -0,0 +1,70 @@
+﻿//Task41
+
+
+
+Console.Write("input numbers in a row with commas: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"count of numbers more than 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("input b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($" X: {x}, Y: {y}");
